7f63204a6f2917f68aa8dfc4e321d579a4be9a46
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-12-02 14:35:35 -0300

    feature: create SecurityConfig

---
+++ src/main/java/com/josiasmartins/apinaruto/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.josiasmartins.apinaruto.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.josiasmartins.apinaruto.security.jwt.JwtAuthFilter;
+import com.josiasmartins.apinaruto.security.jwt.JwtService;
+import com.josiasmartins.apinaruto.services.Impl.UsuarioServiceImpl;
+
+@EnableWebSecurity // implementa o @Configuration
+public class SecurityConfig {
+
+    // $2a$12$IFBpq3I3jec4FdfwTDzmGOCdGCCuySnCzKZbeElUFebvdHtD192v2
+    // $2a$12$UhyS80bl0Eszdj4st9m/R.TpSNJI2c/hRSKna5t2VXd48zizuToPu
+
+    @Autowired
+    private UsuarioServiceImpl usuarioServiceImpl;
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+       return new BCryptPasswordEncoder(); // descryptografa e cryptografa
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter() {
+        return new JwtAuthFilter(jwtService, usuarioServiceImpl);
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth
+                .userDetailsService(usuarioServiceImpl)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // csrf: permite que aja um secuguranca entre uma aplicacao web e back
+                .authorizeRequests()
+                    .requestMatchers("/api/clientes/**")
+    //                        .hasAnyAuthority("MANTER_USUARIO");
+    //                    .authenticated()
+    //                    .permitAll()
+                        .hasAnyRole("USER", "ADMIN")
+                    .requestMatchers("/api/pedidos/**")
+                        .hasAnyRole("USER", "ADMIN")
+                    .requestMatchers("/api/produtos/**")
+                        .hasRole("ADMIN")
+
+                    .requestMatchers(HttpMethod.POST, "/api/usuarios/**")
+                        .permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // sem sessão
+                .and()
+                    .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "/webjars/**");
+    }
+
+
+}
